196b9ac0b357402ac1faffeaed0c5d7dbc5cc190
DeveloperSnehal <developer.creativemarque@gmail.com>
2023-06-24 16:53:20 +0530

    add containt all in index page and header footer

---
+++ footer.php
@@ -0,0 +1,96 @@
+<footer class="page_footer ds color section_padding_top_100 section_padding_bottom_65 table_section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-6">
+				<div class="widget widget_text"> <a href="index-2.html"
+						class="logo vertical_logo bottommargin_20">
+						<img src="images\logo\mygenic-logo-tm.png" alt="">
+					</a>
+					<p>Lorem ipsum dlor amet, consetetur sadipscing esed dia nonumy eirmod tempor invidunt.
+					</p>
+					<p class="darklinks">
+                        <a class="social-icon soc-facebook" href="#" title="Facebook"></a>
+						<a class="social-icon soc-twitter" href="#" title="Twitter"></a>
+                        <a class="social-icon soc-whatsapp" href="#" title="whatsapp"></a>
+                        <a class="social-icon soc-linkedin" href="#" title="Twitter"></a>
+                        <a class="social-icon soc-instagram" href="#" title="instagram"></a>
+                    </p>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-sm-6">
+				<div class="widget widget_twitter topmargin_25">
+					<h4 class="widget-title">Our Company</h4>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Our Products</a></li>
+                        <li><a href="#">How We Do?</a></li>
+                        <li><a href="#">Contact Us</a></li>
+
+                    </ul>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-sm-6 text-center text-sm-left">
+				<div class="widget widget_instagram topmargin_25">
+					<h4 class="widget-title">Our Services</h4>
+					<div class="instafeed"> </div>
+                    <ul>
+                        <li><a href="#">Contract Manufacturer</a></li>
+                        <li><a href="#">Third Party Pharma Manufacturer</a></li>
+                        <li><a href="#">Quality Medicine Supplier</a></li>
+                        <li><a href="#">Leading Pharmaceutical Exporter</a></li>
+                        <li><a href="#">Research & Development</a></li>
+                    </ul>
+				</div>
+			</div>
+
+            <div class="col-md-3 col-sm-6">
+				<div class="widget widget_text topmargin_25">
+					<h4 class="widget-title">Contact Us</h4>
+					<div class="media">
+						<div class="media-left rightpadding_10"> <i class="fa fa-map-marker highlight"
+								aria-hidden="true"></i> </div>
+						<div class="media-body"> Office No- 2 Plot No- 98, Sector 35 Dolphin Platinum Kamothe, Navi Mumbai, Maharashtra, India </div>
+					</div>
+					<div class="media">
+						<div class="media-left rightpadding_10"> <i class="fa fa-phone highlight"
+								aria-hidden="true"></i> </div>
+						<div class="media-body"> +91 9867592446 </div>
+					</div>
+					<div class="media">
+						<div class="media-left rightpadding_10">
+                            <i class="fa fa-pencil highlight" aria-hidden="true"></i>
+                         </div>
+						<div class="media-body highlight2links">
+                            <a href="#"><span>info@mygenichealthcare.com</span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+			<section class="ds color darker page_copyright section_padding_15">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<p class="fontsize_12">&copy; Copyright 2023. MyGenic Healthcare Pvt Ltd, All Rights Reserved. With Love by <a href="https://www.creativemarque.com/"><b>Creativemarque</b></a></p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+		<!-- eof #box_wrapper -->
+	</div>
+	<!-- eof #canvas -->
+	<script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+	<script src="js/compressed.js"></script>
+	<script src="js/main.js"></script>
+</body>
+
+
+<!-- Mirrored from html.modernwebtemplates.com/pharma/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 23 Jun 2023 12:50:52 GMT -->
+
+</html>
---
+++ header.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+
+<!-- Mirrored from html.modernwebtemplates.com/pharma/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 23 Jun 2023 12:49:57 GMT -->
+<head>
+	<title>MyGenic Healthcare</title>
+	<meta charset="utf-8">
+	<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<![endif]-->
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/animations.css">
+	<link rel="stylesheet" href="css/fonts.css">
+	<link rel="stylesheet" href="css/main.css" class="color-switcher-link">
+	<link rel="stylesheet" href="css/shop.css">
+    <link href="images\logo\mygenic_favicon.png" rel="icon">
+	<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+	<!--[if lt IE 9]>
+		<script src="js/vendor/html5shiv.min.js"></script>
+		<script src="js/vendor/respond.min.js"></script>
+		<script src="js/vendor/jquery-1.12.4.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<!--[if lt IE 9]>
+		<div class="bg-danger text-center">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" class="highlight">upgrade your browser</a> to improve your experience.</div>
+	<![endif]-->
+	<div class="preloader">
+		<div class="preloader_image"></div>
+	</div>
+	<!-- search modal -->
+	<div class="modal" tabindex="-1" role="dialog" aria-labelledby="search_modal" id="search_modal"> <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		<span aria-hidden="true">
+			<i class="rt-icon2-cross2"></i>
+		</span>
+	</button>
+		<div class="widget widget_search">
+			<form method="get" class="searchform search-form form-inline" action="https://html.modernwebtemplates.com/pharma/">
+				<div class="form-group bottommargin_0"> <input type="text" value="" name="search" class="form-control" placeholder="Search keyword" id="modal-search-input"> </div> <button type="submit" class="theme_button">Search</button> </form>
+		</div>
+	</div>
+	<!-- Unyson messages modal -->
+	<div class="modal fade" tabindex="-1" role="dialog" id="messages_modal">
+		<div class="fw-messages-wrap ls with_padding">
+			<!-- Uncomment this UL with LI to show messages in modal popup to your user: -->
+			<!--
+		<ul class="list-unstyled">
+			<li>Message To User</li>
+		</ul>
+		-->
+		</div>
+	</div>
+	<!-- eof .modal -->
+	<!-- wrappers for visual page editor and boxed version of template -->
+	<div id="canvas">
+		<div id="box_wrapper">
+			<!-- template sections -->
+			<section class="page_topline cs main_color2 table_section table_section_sm section_padding_top_5 section_padding_bottom_5">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm text-center text-sm-right greylinks"> <span class="rightpadding_10">Follow Us:</span>
+                          <a class="social-icon color-icon soc-facebook" href="#" title="Facebook"></a>
+                          <a class="social-icon color-icon soc-twitter" href="#" title="Twitter"></a>
+                          <a class="social-icon color-icon soc-whatsapp" href="#" title="whatsapp"></a>
+                          <a class="social-icon color-icon soc-linkedin" href="#" title="Twitter"></a>
+                          <a class="social-icon color-icon soc-instagram" href="#" title="instagram"></a>
+                        </div>
+					</div>
+				</div>
+			</section>
+			<section class="page_toplogo table_section table_section_md ls section_padding_top_25 section_padding_bottom_25">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 text-center text-md-left"> <a href="index.php" class="logo top_logo">
+                    <img src="images\logo\mygenic_logo.png" alt="">
+                </a> </div>
+						<div class="col-md-9 text-center text-md-right">
+							<div class="inline-teasers-wrap">
+								<div class="teaser small-teaser media">
+									<div class="media-left media-middle">
+										<div class="teaser_icon size_small border_icon highlight2 rounded"> <i class="rt-icon2-pen"></i> </div>
+									</div>
+									<div class="media-body media-middle">
+										<h4>+91 9867592446</h4>
+										<p class="greylinks fontsize_12"> <a href="#"><span class="__cf_email__" data-cfemail="64140c05160905070b0924011c05091408014a070b09">info@mygenichealthcare.com</span></a> </p>
+									</div>
+								</div>
+								<div class="teaser small-teaser media">
+									<div class="media-left media-middle">
+										<div class="teaser_icon size_small border_icon highlight2 rounded"> <i class="rt-icon2-map-pin"></i> </div>
+									</div>
+									<div class="media-body media-middle">
+										<h4>Office No- 2</h4>
+										<p class="greylinks fontsize_12"> Navi Mumbai, Maharashtra, India </p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<header class="page_header header_color toggler_left">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 display_table">
+							<div class="header_mainmenu display_table_cell">
+								<!-- main nav start -->
+								<nav class="mainmenu_wrapper">
+									<ul class="mainmenu nav sf-menu">
+										<li class="active"> <a href="index.php">Home</a></li>
+										<li> <a href="about.html">About Us</a></li>
+										<!-- eof pages -->
+										<li> <a href="pipeline.html">Our Products</a></li>
+										<!-- gallery -->
+										<li> <a href="gallery-regular.html">Services</a>
+                                          <ul>
+												<li> <a href="#">Contract Manufacturer</a> </li>
+												<li> <a href="#">Third Party Pharma Manufacturer</a> </li>
+												<li> <a href="#">Quality Medicine Supplier</a> </li>
+												<li> <a href="#">Leading Pharmaceutical Exporter</a> </li>
+												<li> <a href="#">Research And Development</a> </li>
+											</ul>
+										</li>
+										<!-- eof Gallery -->
+										<!-- blog -->
+										<li> <a href="blog-right.html">How We Do?</a></li>
+										<!-- eof blog -->
+										<!-- contacts -->
+										<li> <a href="contact.html">Contacts</a></li>
+										<!-- eof contacts -->
+									</ul>
+								</nav>
+								<!-- eof main nav -->
+								<!-- header toggler --><span class="toggle_menu"><span></span></span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
