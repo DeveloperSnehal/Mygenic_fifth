2c262040df1c84674972fac0e1d2e43ab594e209
DeveloperSnehal <developer.creativemarque@gmail.com>
2023-06-26 12:08:52 +0530

    add section in about-us page

--- about-us.php
+++ about-us.php
@@ -8,8 +8,8 @@
 				<h2>About Us</h2>
 				<ol class="breadcrumb highlightlinks">
 					<li>
-                        <a href="index-2.html">Home</a>
-                    </li>
+						<a href="index-2.html">Home</a>
+					</li>
 					<li class="active">About Us</li>
 				</ol>
 			</div>
@@ -22,21 +22,344 @@
 		<div class="row">
 			<div class="col-sm-12"> <img src="images/gallery/06.jpg" alt="" class="alignleft">
 				<h2 class="section_header">About Us</h2>
-				<p>Ball tip tri-tip landjaeger boudin beef ribs shoulder. Cow shank landjaeger, porchetta pancetta chicken doner prosciutto short loin short ribs turducken brisket filet mignon beef ribs. Drumstick strip steak turducken shankle, jowl pork belly landjaeger
-					t-bone. Venison alcatra shankle kielbasa shank. Hamburger salami bresaola filet mignon doner frankfurter. Shoulder chicken sirloin, leberkas pig jerky tail meatball.</p>
-				<div class="inline-block topmargin_30">
-					<ul class="list2 checklist color2 grey">
-						<li>Tenderloin boudin rump capicola </li>
-						<li>Ball tip turkey tri-tip drumstick sho</li>
-						<li>Ribs bacon swine venison corned</li>
-					</ul>
-				</div>
-				<p>Ground round doner meatloaf turducken, jowl meatball chuck ham hock corned beef pork belly rump porchetta tail. Pancetta flank swine, shankle pork chop hamburger picanha capicola burgdoggen boudin. Ground round cupim turkey boudin pork andouille.
-					Shoulder tongue biltong hamburger, cow filet mignon t-bone corned beef kielbasa jowl. Capicola biltong ham hock doner bacon short ribs meatloaf salami. Bacon spare ribs kielbasa pastrami beef. Ground round burgdoggen doner chicken short ribs,
-					jerky biltong pork loin shank picanha flank corned beef rump pig. </p>
+				<p>We are a leading pharmaceutical company dedicated to providing high-quality products and services to
+					businesses in the healthcare industry. With our extensive expertise, cutting-edge technologies and
+					commitment to excellence, we aim to be your trusted partner in achieving success and improving
+					patient outcomes.</p>
+				<p>We are one of the leading Exporter of a premium range of Pharmaceutical Tablets, Pharmaceutical
+					Injection & Anti Cancer Capsules and many more.</p>
+				<p><b>We have experties in pharmaceutical products from 25 years!</b></p>
 			</div>
 		</div>
 	</div>
 </section>
+
+<section id="products"
+	class="ds color parallax page_gallery section_padding_top_100 section_padding_bottom_150 columns_padding_25">
+	<div class="container">
+		<div class="row display_table_md">
+			<div class="col-md-6 col-md-push-6 display_table_cell_md">
+				<h2 class="section_header">Our Vision</h2>
+				<p class="highlight bottommargin_20"><strong>What We Aspire Today</strong></p>
+				<p>Our vision at MyGenic Healthcare Private Limited is to become a global leader in the pharmaceutical
+					industry, known for our excellence in research and development, quality products, and
+					customer-centric approach. We aim to improve the lives of patients and make a significant
+					contribution to global healthcare through our commitment to innovation, sustainability, and social
+					responsibility.</p>
+			</div>
+			<div class="col-md-6 col-md-pull-6 display_table_cell_md">
+				<h2 class="section_header">Our Mission</h2>
+				<p class="highlight bottommargin_20"><strong>Who We Are What We Do</strong></p>
+				<p>Our mission at MyGenic Healthcare Private Limited is to provide high-quality, affordable
+					pharmaceutical products to patients around the world. We are committed to developing innovative
+					solutions to complex health challenges and improving patient outcomes through research and
+					development, clinical trials, and partnerships with healthcare professionals.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="ls section_padding_top_100 section_padding_bottom_50">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<h2>Why Choose Us?</h2>
+				<p>The key points for which we have won noise from our buyers include:</p>
+				<hr>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="teaser text-center">
+					<div class="teaser_icon main_bg_color size_normal round"> <i class="fa fa-comments-o"></i> </div>
+					<h3 class="text-uppercase">Experience & Core Knowledge</h3>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="teaser text-center">
+					<div class="teaser_icon main_bg_color size_normal round"> <i class="fa fa-star-o"></i> </div>
+					<h3 class="text-uppercase"> CLIENT-CENTRIC APPROACH </h3>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="teaser text-center">
+					<div class="teaser_icon main_bg_color size_normal round"> <i class="fa fa-thumbs-o-up"></i> </div>
+					<h3 class="text-uppercase"> FAIR DEALINGS </h3>
+				</div>
+			</div>
+		</div>
+		<div class="row" style="display: flex; justify-content: center;">
+			<div class="col-sm-4">
+				<div class="teaser text-center">
+					<div class="teaser_icon main_bg_color size_normal round"> <i class="fa fa-comments-o"></i> </div>
+					<h3 class="text-uppercase"> ETHICAL BUSINESS DEALINGS </h3>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="teaser text-center">
+					<div class="teaser_icon main_bg_color size_normal round"> <i class="fa fa-star-o"></i> </div>
+					<h3 class="text-uppercase"> COMPETITIVE PRICING </h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!--------------------------------FACTSHEET---------------------------------------------->
+<section id="ourproducts" style="background-color:#fff;">
+	<div class="container">
+		<div class="row">
+			<!-------------second-colum----------------->
+			<div class="col-lg-12 col-md-12 col-sm-12 mt-2 card" data-aos="flip-left" data-aos-easing="ease-out-cubic"
+				data-aos-duration="2000">
+				<div class="row">
+					<h3 class="text-center pt-5" style="padding-top: 50px;">FACTSHEET</h3>
+					<div class="container">
+						<div class="row px-5">
+							<h5 class="fw-bolder text-decoration-underline">Basic Information</h5>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">Nature of Business:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p>Exporter</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">Additional Business:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p>
+									Wholesaler<br>
+									Distributor<br>
+									Service Provider<br>
+									Manufacturer
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">Company CEO:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p>Namrata Patil</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">Registered Address:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p>Office No- 2 Plot No- 98, Sector 35 Dolphin Platinum Kamothe, Navi Mumbai, Raigarh,
+									Navi Mumbai - 410209, Maharashtra, India</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">Total Number of Employees:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p>Upto 10 People</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">Year of Establishment:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p>2022</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">Legal Status of Firm:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p>Limited Company (Ltd. / Pvt. Ltd.)</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">Annual Turnover:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p>Rs. 10 - 25 Crore</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<h5 class="fw-bolder text-decoration-underline">Trade & Market </h5>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold"> Export Percentage:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p> upto 20%</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<h5 class="fw-bolder text-decoration-underline">Statutory Profile</h5>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">Import Export Code (IEC):</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p> AATCA*****</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold"> Tan No.:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p> BPLA1***** </p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">GST No.:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p>27AATCA8561A1ZH </p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">CIN No.:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p>U85100MH2020PTC343851</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<h5 class="fw-bolder text-decoration-underline">Packaging/Payment and Shipment Details</h5>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">Payment Mode:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p>Cash<br>Credit Card <br> Bank Transfer <br> Cheque <br> DD <br> Online <br></p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row px-5">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p class="fw-bold">Shipment Mode:</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								<p> By Road</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+</section>
+
+<section class="ls section_padding_top_150 section_padding_bottom_150">
+   <div class="row">
+        <div class="col-sm-12 text-center" style="padding-top: 70px;">
+          <h2 class="section_header"> Certificates </h2>
+        </div>
+    </div>
+	<div class="container" style="padding-top:0px;">
+		<div class="row">
+			<div class="col-md-12">
+				<div id="gallery-owl-carousel" class="owl-carousel dots-center with_border_items" data-dots="true"
+					data-items="3" data-responsive-lg="3" data-responsive-md="2" data-responsive-sm="2"
+					data-responsive-xs="1">
+					<article
+						class="vertical-item content-padding big-padding post format-standard with_border rounded text-center">
+						<div class="item-media"> <img src="images/gallery/01.jpg" alt="">
+							<div class="media-links">
+								<div class="links-wrap"> <a class="p-view prettyPhoto " title=""
+										data-gal="prettyPhoto[gal]" href="images/gallery/01.jpg"></a> </div>
+							</div>
+						</div>
+					</article>
+					<article
+						class="vertical-item content-padding big-padding post format-standard with_border rounded text-center">
+						<div class="item-media"> <img src="images/gallery/02.jpg" alt="">
+							<div class="media-links">
+								<div class="links-wrap"> <a class="p-view prettyPhoto " title=""
+										data-gal="prettyPhoto[gal]" href="images/gallery/02.jpg"></a> </div>
+							</div>
+						</div>
+					</article>
+					<article
+						class="vertical-item content-padding big-padding post format-standard with_border rounded text-center">
+						<div class="item-media"> <img src="images/gallery/03.jpg" alt="">
+							<div class="media-links">
+								<div class="links-wrap"> <a class="p-view prettyPhoto " title=""
+										data-gal="prettyPhoto[gal]" href="images/gallery/03.jpg"></a> </div>
+							</div>
+						</div>
+					</article>
+					<article
+						class="vertical-item content-padding big-padding post format-standard with_border rounded text-center">
+						<div class="item-media"> <img src="images/gallery/04.jpg" alt="">
+							<div class="media-links">
+								<div class="links-wrap"> <a class="p-view prettyPhoto " title=""
+										data-gal="prettyPhoto[gal]" href="images/gallery/04.jpg"></a> </div>
+							</div>
+						</div>
+					</article>
+					<article
+						class="vertical-item content-padding big-padding post format-standard with_border rounded text-center">
+						<div class="item-media"> <img src="images/gallery/05.jpg" alt="">
+							<div class="media-links">
+								<div class="links-wrap"> <a class="p-view prettyPhoto " title=""
+										data-gal="prettyPhoto[gal]" href="images/gallery/05.jpg"></a>
+								</div>
+							</div>
+						</div>
+					</article>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 
 <?php include("footer.php") ?>
